9823004cb3a9d2eae751d9c90825a861d73bafaf
Craig Sheen <craig@pauldurbin.co.uk>
2013-01-29 19:06:03 +0000

    Changed return to be a hash

--- lib/credit_card_validator.rb
+++ lib/credit_card_validator.rb
@@ -4,20 +4,21 @@ module CreditCardValidator
   
   def self.validate(number)
     number = number.to_s.gsub(/\s+/, "")
+    check = {}
     if (number[0,2] == "34" || number[0,2] == "37") && number.length == 15
-      check = "AMEX: "
+      check[:type] = "AMEX"
     elsif number[0,4] == "6011" && number.length == 16
-      check = "Discover: "
+      check[:type] = "Discover"
     elsif (number[0,2] == "51" || number[0,2] == "52" || number[0,2] == "53" || number[0,2] == "54" || number[0,2] == "55") && number.length == 16
-      check = "Mastercard: "
+      check[:type] = "Mastercard"
     elsif number [0,1] == "4" && (number.length == 13 || number.length == 14 || number.length == 15 || number.length == 16)
-      check = "VISA: "
+      check[:type] = "VISA"
     else
-      check = "Unknown: #{number} (Invalid)"
+      check[:error] = "Unknown card type"
       return check
     end
     
-    check += number
+    check[:number] = number
     
     total = 0
     number.reverse.split("").each_with_index do |n,index|
@@ -29,9 +30,9 @@ module CreditCardValidator
     end
 
     if total % 10 == 0
-      check += "(Valid)"
+      check[:valid] = "Valid"
     else
-      check += "(Invalid)"
+      check[:valid] = "Invalid"
     end
     
     return check
